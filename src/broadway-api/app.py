bebeb014b6e74ee56c6ae7c0a68b05cd858721ee
rhidalgo531 <raify.hidalgo@1stdibs.com>
2019-01-30 19:13:34 -0500

    Add static files for later use in S3

--- src/broadway-api/app.py
+++ src/broadway-api/app.py
@@ -1,11 +1,32 @@
-from chalice import Chalice
+from chalice import Chalice, Response
+
+
+
 
 app = Chalice(app_name='broadway-api')
+app.debug = True
 
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+
+
+
+
+"""
+    This endpoint will return information regarding broadway performances
+
+    In a larger API, performances would be the collection, and broadway could be one of multiple resources
+    but for this broadway will be the only one
+
+
+"""
+@app.route('/performances/broadway/{year}', methods=["GET"]):
+def return_dataset_by_year(year):
+    
+
 
 
 # The view function above will return {"hello": "world"}
