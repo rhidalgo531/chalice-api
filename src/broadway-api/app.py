538abe189570236737ccb5d5dc9c326827b6368d
rhidalgo531 <raify.hidalgo@1stdibs.com>
2019-01-30 20:56:43 -0500

    Start on index page

--- src/broadway-api/app.py
+++ src/broadway-api/app.py
@@ -57,28 +57,3 @@ def return_dataset_by_year(year):
             return Response(status_code=200, body=response_body, headers=headers)
         else:
             return Response(status_code=dataset.status_code, body=dataset.data, headers={"Content-Type":"application/json"})
-
-
-
-
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
